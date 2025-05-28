5ef29246f7a46a49664d527836c56e15fa743c37
akshithanaverse <akshithanaverse@gmail.com>
2025-05-28 22:10:08 +0530

    Tailwind addition and inital setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline decoration-1 text-red-600'>
+        Hello World!
+      </h1>
+    </>
+  )
+}
+
+export default App
