bdf4feff549cd603791de3ebd6443de8a3849b27
akshithanaverse <akshithanaverse@gmail.com>
2025-05-29 22:01:14 +0530

    Configured vercel.json

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function StartButton() {
+    const navigate = useNavigate();
     return(
-        <button>Start</button>
+        <button onClick={()=> navigate("/Game")}>Start</button>
     )
 }
 
